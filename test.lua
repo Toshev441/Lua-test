728a271965120a854ee31bb116124e77bb74ed01
Alex <toshev441@gmail.com>
2025-01-02 11:12:28 +0000

    sock

--- test.lua
+++ test.lua
@@ -1,1 +1,13 @@
 print('hi')
+
+local socket = require('socket')
+local sock = socket.tcp4()
+
+sock:bind("*", 8080)
+sock:listen(12)
+local client = sock:accept()
+if client then
+    data = client:receive()
+    print(data)
+
+end
